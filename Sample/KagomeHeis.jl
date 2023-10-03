a7069371fd8297136b47c28e9332cc7865c4ddfa
Anjishnu Bose <anjishnubose98@gmail.com>
2023-10-03 15:43:27 -0400

    Updated sample code

--- Sample/KagomeHeis.jl
+++ Sample/KagomeHeis.jl
@@ -1,89 +1,84 @@
-include("../src/MeanFieldToolkit.jl")
-using .MeanFieldToolkit
+using MeanFieldToolkit, LinearAlgebra, TightBindingToolkit, Distributions, LaTeXStrings
 
-using LinearAlgebra, TightBindingToolkit, Distributions, LaTeXStrings
-##### Kagome lattice
+########## Defining Kagome lattice with the doubled unit cell
+##### Primitives
 const a1  =   [4.0, 0.0]
 const a2  =   [1.0, sqrt(3)]
-
+##### 6 sublattices
 const b1  =   [0.0, 0.0]
 const b2  =   [1.0, 0.0] 
 const b3  =   [0.5, sqrt(3)/2]  
 const b4  =   [2.0 + 0.0, 0.0]
 const b5  =   [2.0 + 1.0, 0.0] 
 const b6  =   [2.0 + 0.5, sqrt(3)/2]
-
+##### On-site spin matrices
 const InitialField  =   zeros(Float64, 4)
 const OnSite        =   SpinMats(1//2)
-
-HoppingUC       =   UnitCell([a1, a2], 2, 2)
-InteractionUC   =   UnitCell([a1, a2], 2, 4)
-AddBasisSite!.(Ref(HoppingUC),     [b1, b2, b3, b4, b5, b6], Ref(InitialField) , Ref(OnSite))
-AddBasisSite!.(Ref(InteractionUC), [b1, b2, b3, b4, b5, b6], Ref(InitialField) , Ref(OnSite))
-
+##### Unit cell with local dimensions of 2, and tracking rank-2 bonds
+UC       =   UnitCell([a1, a2], 2, 2)
+AddBasisSite!.(Ref(UC),     [b1, b2, b3, b4, b5, b6], Ref(InitialField) , Ref(OnSite))
+##### Strength of the Heisenberg interaction
 const J     =   1.0
+##### Heisenberg spin exchange matrix
 Jmatrix     =   Matrix{Float64}(I, 3, 3)
+##### Heisenberg interaction written in terms of 4-parton interactions --> rank-4 array.
 U           =   SpinToPartonCoupling(Jmatrix, 1//2)
-###### Interaction Block                                  
+###### Interaction parameter which is isotropic.                               
 JParam      =   Param(J, 4)
-AddIsotropicBonds!(JParam, InteractionUC, 1.0, U, "Heisenberg Interaction")
-InteractionBlock   =  ParamBlock([JParam], InteractionUC)
-
-const n       =   20
+AddIsotropicBonds!(JParam, UC, 1.0, U, "Heisenberg Interaction")
+Interactions=   [JParam]
+##### Brillouin zone
+const n       =   10
 const kSize   =   6 * n + 3  
 bz            =   BZ([kSize, kSize])
-FillBZ!(bz, HoppingUC)
-
+FillBZ!(bz, UC)
 ##### Thermodynamic parameters
 const T         =   0.001
 const filling   =   0.5
 const stat      =   -1
-
+##### Mixing alpha used for self-consistency solver
 const mixingAlpha    =    0.5  
-
+##### Different flux configuration ansatzes to be tested.
 phis            =   collect(LinRange(-pi/2, pi/2, 21))
 
 for ϕ in phis
 
-    ChiUC           =   UnitCell([a1, a2], 2, 2)
-
-    AddBasisSite!.(Ref(ChiUC), [b1, b2, b3, b4, b5, b6], Ref(InitialField), Ref(OnSite))
-
     ##### Hopping expectation params
-    # Hopping with flux ϕ through the triangles, and π-2ϕ through the hexagons
+    ##### Hopping with flux ϕ through the triangles, and π-2ϕ through the hexagons
     t_flux  =   Param(1.0, 2)
-    AddAnisotropicBond!(t_flux, ChiUC, 1, 2, [  0,  0],  exp( im * ϕ/3) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
-    AddAnisotropicBond!(t_flux, ChiUC, 2, 3, [  0,  0],  exp( im * ϕ/3) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
-    AddAnisotropicBond!(t_flux, ChiUC, 3, 1, [  0,  0],  exp( im * ϕ/3) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
-    AddAnisotropicBond!(t_flux, ChiUC, 4, 5, [  0,  0],  exp( im * ϕ/3) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
-    AddAnisotropicBond!(t_flux, ChiUC, 5, 6, [  0,  0],  exp( im * ϕ/3) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
-    AddAnisotropicBond!(t_flux, ChiUC, 6, 4, [  0,  0],  exp( im * ϕ/3) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
-    AddAnisotropicBond!(t_flux, ChiUC, 4, 2, [  0,  0],  exp( im * ϕ/3) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
+    AddAnisotropicBond!(t_flux, UC, 1, 2, [  0,  0],  exp( im * ϕ/3) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
+    AddAnisotropicBond!(t_flux, UC, 2, 3, [  0,  0],  exp( im * ϕ/3) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
+    AddAnisotropicBond!(t_flux, UC, 3, 1, [  0,  0],  exp( im * ϕ/3) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
+    AddAnisotropicBond!(t_flux, UC, 4, 5, [  0,  0],  exp( im * ϕ/3) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
+    AddAnisotropicBond!(t_flux, UC, 5, 6, [  0,  0],  exp( im * ϕ/3) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
+    AddAnisotropicBond!(t_flux, UC, 6, 4, [  0,  0],  exp( im * ϕ/3) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
+    AddAnisotropicBond!(t_flux, UC, 4, 2, [  0,  0],  exp( im * ϕ/3) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
 
-    AddAnisotropicBond!(t_flux, ChiUC, 2, 6, [  0, -1],  exp( im * (pi + ϕ/3)) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
-    AddAnisotropicBond!(t_flux, ChiUC, 4, 6, [  0, -1],  exp(-im * (pi + ϕ/3)) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
-    AddAnisotropicBond!(t_flux, ChiUC, 5, 3, [  1, -1],  exp( im * (pi + ϕ/3)) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
-    AddAnisotropicBond!(t_flux, ChiUC, 5, 1, [  1,  0],  exp(-im * (pi + ϕ/3)) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
-    AddAnisotropicBond!(t_flux, ChiUC, 3, 1, [  0,  1],  exp( im * ϕ/3) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
+    AddAnisotropicBond!(t_flux, UC, 2, 6, [  0, -1],  exp( im * (pi + ϕ/3)) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
+    AddAnisotropicBond!(t_flux, UC, 4, 6, [  0, -1],  exp(-im * (pi + ϕ/3)) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
+    AddAnisotropicBond!(t_flux, UC, 5, 3, [  1, -1],  exp( im * (pi + ϕ/3)) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
+    AddAnisotropicBond!(t_flux, UC, 5, 1, [  1,  0],  exp(-im * (pi + ϕ/3)) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
+    AddAnisotropicBond!(t_flux, UC, 3, 1, [  0,  1],         exp( im * ϕ/3) * OnSite[4], 1.0, "[ϕ, ϕ, π - 2*ϕ] Hopping with ϕ/π = $(ϕ/pi)")
 
-    ######### Weiss fields
+    ######### Weiss fields for ordering
     Ordering    =   Param(1.0, 2)
-    AddAnisotropicBond!(Ordering, ChiUC, 1, 1, [ 0, 0],  sum([ -sqrt(3)/2, -1/2, 0.0] .* OnSite[1:3]), 0.0, "120 degree ordering")
-    AddAnisotropicBond!(Ordering, ChiUC, 2, 2, [ 0, 0],  sum([  sqrt(3)/2, -1/2, 0.0] .* OnSite[1:3]), 0.0, "120 degree ordering")
-    AddAnisotropicBond!(Ordering, ChiUC, 3, 3, [ 0, 0],  sum([        0.0,  1.0, 0.0] .* OnSite[1:3]), 0.0, "120 degree ordering")
-    AddAnisotropicBond!(Ordering, ChiUC, 4, 4, [ 0, 0],  sum([ -sqrt(3)/2, -1/2, 0.0] .* OnSite[1:3]), 0.0, "120 degree ordering")
-    AddAnisotropicBond!(Ordering, ChiUC, 5, 5, [ 0, 0],  sum([  sqrt(3)/2, -1/2, 0.0] .* OnSite[1:3]), 0.0, "120 degree ordering")
-    AddAnisotropicBond!(Ordering, ChiUC, 6, 6, [ 0, 0],  sum([        0.0,  1.0, 0.0] .* OnSite[1:3]), 0.0, "120 degree ordering")
+    AddAnisotropicBond!(Ordering, UC, 1, 1, [ 0, 0],  sum([ -sqrt(3)/2, -1/2, 0.0] .* OnSite[1:3]), 0.0, "120 degree ordering")
+    AddAnisotropicBond!(Ordering, UC, 2, 2, [ 0, 0],  sum([  sqrt(3)/2, -1/2, 0.0] .* OnSite[1:3]), 0.0, "120 degree ordering")
+    AddAnisotropicBond!(Ordering, UC, 3, 3, [ 0, 0],  sum([        0.0,  1.0, 0.0] .* OnSite[1:3]), 0.0, "120 degree ordering")
+    AddAnisotropicBond!(Ordering, UC, 4, 4, [ 0, 0],  sum([ -sqrt(3)/2, -1/2, 0.0] .* OnSite[1:3]), 0.0, "120 degree ordering")
+    AddAnisotropicBond!(Ordering, UC, 5, 5, [ 0, 0],  sum([  sqrt(3)/2, -1/2, 0.0] .* OnSite[1:3]), 0.0, "120 degree ordering")
+    AddAnisotropicBond!(Ordering, UC, 6, 6, [ 0, 0],  sum([        0.0,  1.0, 0.0] .* OnSite[1:3]), 0.0, "120 degree ordering")
 
-    ChiParams    =  [t_flux, Ordering]
-    HoppingBlock =  ParamBlock(ChiParams, ChiUC)
-
-    H             =   Hamiltonian(HoppingUC, bz)
+    HoppingOrders =  [t_flux, Ordering]
+    ##### Build the model (trivial since we are looking at the pure spin  Heisenberg model)
+    H             =   Hamiltonian(UC, bz)
     DiagonalizeHamiltonian!(H)
-    M             =   Model(HoppingUC, bz, H ; T=T, filling=filling, stat=stat)
-
-    mft            =   TightBindingMFT(M, HoppingBlock, InteractionBlock, InterQuarticToHopping, Dict{String, Float64}("ij" => 1.0, "ii" => 0.0, "jj" => 0.0) )
-    fileName    =   "./Sample/KagomeDiracSL/J=$(round(J, digits=3))_phi=$(round(ϕ/pi, digits=3))Pi_New.jld2"
+    M             =   Model(UC, bz, H ; T=T, filling=filling, stat=stat)
+    ##### Build the mean-field theory object, with chosen scaling such that on-site ordering is suppressed.
+    mft            =   TightBindingMFT(M, HoppingOrders, Interactions, Function[InterQuarticToHopping], Dict{String, Float64}("ij" => 1.0, "ii" => 0.0, "jj" => 0.0) )
+    ##### File to save data to
+    fileName    =   "./KagomeHeis_Data/J=$(round(J, digits=3))_phi=$(round(ϕ/pi, digits=3))Pi_New.jld2"
+    ##### Solve the mean-field theory and save the results in fileName
     SolveMFT!(mft, fileName)
 
 end
