9e45f0e8ab6faf8fae48da0bca4024d9362e5798
Anjishnu Bose <anjishnubose98@gmail.com>
2023-10-03 11:22:54 -0400

    added documentation

--- src/InteractionConvert.jl
+++ src/InteractionConvert.jl
@@ -3,6 +3,16 @@ module InteractionConvert
 
     using Tullio, LinearAlgebra, TightBindingToolkit
 
+
+@doc """
+```julia
+SpinToPartonCoupling(J::Matrix{Float64}, spin::Rational) --> Array{ComplexF64, 4}
+SpinToPartonCoupling(J::Matrix{Float64}, SpinVec_A::Vector{Matrix{ComplexF64}}, SpinVec_B::Vector{Matrix{ComplexF64}}) --> Array{ComplexF64, 4}
+
+```
+Converts the exchange matrix `J` into a parton coupling array `U` using the spin matrices `SpinVec_A` and `SpinVec_B`.
+If `spin` is passed, then the spin matrices are generated using the `SpinMats` function.
+"""
     function SpinToPartonCoupling(J::Matrix{Float64}, spin::Rational) :: Array{ComplexF64, 4}
 
         SpinVec         =   SpinMats(spin)[1: end - 1]
@@ -11,7 +21,6 @@ module InteractionConvert
 
         return U
     end
-
 
     function SpinToPartonCoupling(J::Matrix{Float64}, SpinVec_A::Vector{Matrix{ComplexF64}}, SpinVec_B::Vector{Matrix{ComplexF64}}) :: Array{ComplexF64, 4}
 
@@ -22,6 +31,12 @@ module InteractionConvert
     end
 
 
+@doc """
+```julia
+DensityToPartonCoupling(Ui::Matrix{Float64}, Uj::Matrix{Float64}) --> Array{ComplexF64, 4}
+```
+Converts the density-density interaction matrix `Ui` and `Uj` into a parton coupling array `U`.
+"""
     function DensityToPartonCoupling(Ui::Matrix{Float64}, Uj::Matrix{Float64}) :: Array{ComplexF64, 4}
         @tullio U[a, b, c, d] := Ui[a, b] * Uj[c, d]
         return U
